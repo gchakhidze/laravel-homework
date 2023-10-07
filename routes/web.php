7d9d7517eb7d6c14bdbbe02b72b7999bc599194a
student <student@BTU.local>
2023-10-07 14:25:35 +0400

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/firstname', function () {
+    return 'giorgi';
+});
+
+Route::get('/lastname', function () {
+    return 'chakhidze';
+});
+
+Route::get('/age', function () {
+    return '23';
+});
+
+Route::get('/hobby', function () {
+    return 'gaming';
+});
+
+Route::get('/language', function () {
+    return 'php';
+});
+
+Route::post('/message', function () {
+    $array = [
+        'message' => "updated successfully"
+    ];
+
+    response() -> json($array);
+});
+
