655b64adb9e84a1037194c1e1d6a43cd15bc79c3
gchakhidze <giorgi.chakhidze.1@btu.edu.ge>
2024-01-03 23:28:44 +0400

    Added breeze auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\QuizController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,18 +9,23 @@ use App\Http\Controllers\QuizController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/testmiddleware/on', function () {
-    return 'my-super-middleware is on';
-})->middleware('mysupermiddleware');
-
-Route::get('/testmiddleware/off', function () {
-    return 'my-super-middleware is off';
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::resource('quizzes', QuizController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
